4e5ed6fd0b56a1ec4f82d2bcdeef7f24cfdc0586
FlatHippo <47155251+justinfarley@users.noreply.github.com>
2026-02-12 16:12:36 -0500

    just some testing, if you think this isn't the coolest thing ever you're wrong

--- NeuralNetwork/test.cs
+++ NeuralNetwork/test.cs
@@ -1,6 +1,6 @@
 using Neural_Network_and_AI;
 
-NeuralNetwork nn = new NeuralNetwork(0.0000001f, "data.csv", 2, 3);
+NeuralNetwork nn = new NeuralNetwork(0.0000001f, "data.csv", 3, 15);
 nn.NetworkInit();
-nn.Train(500);
+nn.Train(35);
 
