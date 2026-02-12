f95a5bb67edc6a0fdd9ebfee480273be2a1482b4
FlatHippo <47155251+justinfarley@users.noreply.github.com>
2026-02-12 16:30:35 -0500

    added comments and documentation

--- NeuralNetwork/test.cs
+++ NeuralNetwork/test.cs
@@ -1,5 +1,19 @@
 using Neural_Network_and_AI;
 
+/// <summary>
+/// Test File!! 
+/// 
+/// Currently creates a NN with learning rate 0.0000001f,
+/// 3 hidden layers each with 15 nodes.
+/// 
+/// It trains for only 35 iterations and precisely predicts the Y values!
+/// 
+/// Adjusting the learning rate and # of hidden nodes can sometimes
+/// cause the network to get stuck in a local minimum, or to exponentially explode with
+/// too low of a learning rate.
+/// 
+/// </summary>
+
 NeuralNetwork nn = new NeuralNetwork(0.0000001f, "data.csv", 3, 15);
 nn.NetworkInit();
 nn.Train(35);
